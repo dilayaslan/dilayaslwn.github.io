0bfc0ce4e28c13332246c64f32453506c6a5674d
dilayaslan <dilaymavi@gmail.com>
2025-03-23 02:17:25 +0300

    Update homework1.c

--- homework1.c
+++ homework1.c
@@ -1,7 +1,22 @@
-include <stdio.h>
+/* Addition program */
+#include <stdio.h>
 
-int main()
+/* function main begins program execution */
+int main( void )
 {
-  printf("dlay");
-  return 0;
-}
+   int integer1;/* first number to be input by user  */
+   int integer2;/* second number to be input by user */
+   int sum;      /* variable in which sum will be stored */ 
+
+   printf("Enter first integer\n" ); /* prompt */
+   scanf( "%d", &integer1 );        /* read an integer */
+
+   printf( "Enter second integer\n" ); /* prompt */
+   scanf( "%d", &integer2 );           /* read an integer */
+   
+   sum = integer1 + integer2; /* assign total to sum */
+
+   printf( "Sum is %d.\n", sum ); /* print sum */
+   return 0; /* indicate that program ended successfully */
+
+} /* end function main */
