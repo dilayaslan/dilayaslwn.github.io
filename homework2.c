ba04bccbdfb7c80ac5ec0730c03498171fe72b5a
dilayaslan <dilaymavi@gmail.com>
2025-03-23 02:32:39 +0300

    Update homework2.c

--- homework2.c
+++ homework2.c
@@ -1,7 +1,14 @@
-include <stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
-  printf("dlay");
+  int popularity, size, homeValue;
+  printf("Enter the popularity of the land:");
+  scanf("%d",&popularity);
+  printf("Enter the size of the house:");
+  scanf("%d",&size);
+  homeValue = ( pow(popularity,3) + pow(size,2)  ) * 10000;
+  printf("The housing price in a particular neighborhood is %d.", homeValue);
   return 0;
 }
